e5eaaaac63c15f64e75ff3c68849e0e444e775f9
tam11a <ibrahimsadiktamim@gmail.com>
2024-06-29 19:49:14 +0600

    back to old code

--- main.py
+++ main.py
@@ -41,9 +41,9 @@ client.on_message = on_message
 
 client.connect(env.mqtt_host, int(env.mqtt_port), 60)
 
-client.loop_start()
-DHT.dht_read(client)
-client.loop_stop()
+# client.loop_start()
+# DHT.dht_read(client)
+# client.loop_stop()
 
 
-# client.loop_forever()
+client.loop_forever()
