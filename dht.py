65a2484fb713686406e701c9cb97cc657f8d8771
tam11a <ibrahimsadiktamim@gmail.com>
2024-06-29 19:44:20 +0600

    updated: Adafruit usage

--- dht.py
+++ dht.py
@@ -3,9 +3,7 @@ import Adafruit_DHT
 import RPi.GPIO as gpio
 import config as env
 import json as JSON
-
-# Initialize the DHT device, with data pin connected to:
-dhtDevice = Adafruit_DHT.DHT22(board.D4, use_pulseio=False)
+import time
 
 
 def dht_read(client):
@@ -13,14 +11,20 @@ def dht_read(client):
         raise Exception('SENSOR_ID is not set')
     while True:
         try:
-            # Print the values to the serial port
-            temperature_c = dhtDevice.temperature
+            sensor = Adafruit_DHT.DHT22
+            humidity, temperature = Adafruit_DHT.read_retry(
+                sensor, env.sensor_id)
+            temperature_c = temperature
             temperature_f = temperature_c * (9 / 5) + 32
-            humidity = dhtDevice.humidity
+
             print({'temperature_c': temperature_c,
                    'temperature_f': temperature_f, 'humidity': humidity})
+
             client.publish(f'sensor/{env.sensor_id}/live', JSON.dumps(
                 {'ref': env.sensor_id, 'temp': temperature_c, 'hum': humidity}))
+
+            time.sleep(5)  # 5 seconds sleep
+
         except RuntimeError as error:
             # Errors happen fairly often, DHT's are hard to read, just keep going
             print(error.args[0])
