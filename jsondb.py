2fbb4bafb50b195b5a89c708c2da3fa4172ecdc5
tam11a <ibrahimsadiktamim@gmail.com>
2024-07-08 02:39:20 +0600

    updated: safe_json functions

--- jsondb.py
+++ jsondb.py
@@ -1,21 +1,21 @@
 import json
 import os
-import threading
+# import threading
 from config import local_file_path as file_path
 
 # Create a lock object
-file_lock = threading.Lock()
+# file_lock = threading.Lock()
 
 
 def safe_write_json(data):
     temp_file_path = file_path + '.tmp'
     try:
-        with file_lock:
-            with open(temp_file_path, 'w') as temp_file:
-                json.dump(data, temp_file)
-                temp_file.flush()
-                os.fsync(temp_file.fileno())
-            os.rename(temp_file_path, file_path)
+        # with file_lock:
+        with open(temp_file_path, 'w') as temp_file:
+            json.dump(data, temp_file)
+            temp_file.flush()
+            os.fsync(temp_file.fileno())
+        os.rename(temp_file_path, file_path)
     except Exception as e:
         print(f"An error occurred: {e}")
         if os.path.exists(temp_file_path):
@@ -23,12 +23,12 @@ def safe_write_json(data):
 
 
 def safe_read_json():
-    with file_lock:
-        if not os.path.exists():
-            return {}
-        try:
-            with open(file_path, 'r') as file:
-                return json.load(file)
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            return {}
+    # with file_lock:
+    if not os.path.exists():
+        return {}
+    try:
+        with open(file_path, 'r') as file:
+            return json.load(file)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return {}
